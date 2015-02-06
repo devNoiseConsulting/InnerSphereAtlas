d5101704e3d5305158dcb480a51552dd7bfd6b4f
Michael J. Flynn <flynnmj@devnoise.com>
2015-02-06 14:30:34 -0500

    Factory Detail conversion

--- lib/product_component.php
+++ lib/product_component.php
@@ -6,29 +6,31 @@ $query = "SELECT DISTINCT
 FROM
 product PC, product_type PT
 WHERE
-PC.factory_id=$factory AND
+PC.factory_id=:factory AND
 PC.product_type_id=PT.product_type_id
 ORDER BY
 PT.product_type_id, PT.product_type, PC.product_name";
-$result_pc = mysql_query($query);
-$num_pc = mysql_numrows($result_pc);
-		
-for ($j = 0; $j < $num_pc; $j++) {
+
+$sth = $dbh->prepare($query);
+$sth->execute(array(':factory' => $factory));
+$components = $sth->fetchAll(PDO::FETCH_ASSOC);
+$sth = null;
+
+for ($j = 0; $j < count($components); $j++) {
 	echo "<tr><td>";
-	$val = mysql_result($result_pc, $j, "product_name");
+	$val = $components[$j]['product_name'];
 	print_sp($val);
 	echo "</td>";
 
-	echo "<td><a href=\"./product-type-detail.php?searchvalue=",urlencode(mysql_result($result_pc,$j,"product_type_id")),"\">";
-	$val = mysql_result($result_pc, $j, "component_type");
+	echo "<td><a href=\"./product-type-detail.php?searchvalue=",urlencode($components[$j]['product_type_id']),"\">";
+	$val = $components[$j]['component_type'];
 	print_sp($val);
 	echo " (";
-	$val = mysql_result($result_pc, $j, "product_type");
+	$val = $components[$j]['product_type'];
 	print_sp($val);
 	echo ")</a></td>";
 
 	echo "</tr>\n";
 }
-mysql_free_result($result_pc);
 ?>
 </table>
