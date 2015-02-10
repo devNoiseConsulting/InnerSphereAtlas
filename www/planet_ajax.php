683f20c15838c184b74a5c64ab61b4189ce5a76a
Michael J. Flynn <flynnmj@devnoise.com>
2015-02-10 14:20:53 -0500

    Autocomplete conversion

--- www/planet_ajax.php
+++ www/planet_ajax.php
@@ -2,25 +2,44 @@
 include("./isatlas-config.php");
 include("$ISA_LIBDIR/connect.php");
 
-$planet = mysql_real_escape_string($_REQUEST["searchvalue"]);
+$searchvalue = array_key_exists("searchvalue", $_REQUEST) ? $_REQUEST["searchvalue"] : "A";
+$searchvalue = trim($searchvalue);
+if (strlen($searchvalue) == 1) {
+	$searchvalue = $searchvalue . "%";
+} else if ($searchvalue{1} <> "%") {
+	$searchvalue = "%" . $searchvalue . "%";
+}
 
-$query = "SELECT DISTINCT P.planet_id, P.name, P.x_coord, P.y_coord FROM planet P
-	WHERE P.name LIKE '%" . $planet . "%' ORDER BY P.name";
+$query = "SELECT DISTINCT
+P.planet_id,
+P.name,
+P.x_coord,
+P.y_coord
+FROM
+planet P
+WHERE
+P.name LIKE :searchvalue
+ORDER BY
+P.name
+";
 
 echo "<ul>\n";
-$result = mysql_query($query) or die (mysql_error () . "<li>The query was:" . $query . "</li>");
-$num = mysql_numrows($result);
+$sth = $dbh->prepare($query);
+$sth->bindParam(':searchvalue', $searchvalue);
+$sth->execute();
+$planetList = $sth->fetchAll(PDO::FETCH_ASSOC);
+$sth = null;
 
+$num = count($planetList);
 if ($num > 0) {
 	if ($num > 15) { $num = 15; }
 	for ($i=0; $i < $num; $i++) {
-		$name = mysql_result($result, $i, "name");
-		$x = mysql_result($result, $i, "x_coord");
-		$y = mysql_result($result, $i, "y_coord");
+		$name = $planetList[$i]['name'];
+		$x = $planetList[$i]['x_coord'];
+		$y = $planetList[$i]['y_coord'];
 	
 		echo "<li>" . $name . "<br/><span class=\"informal\"> (" . $x . ", " . $y . ")<br/></span></li>\n";
 	}
-	mysql_free_result($result);
 	
 }
 echo "</ul>\n";
