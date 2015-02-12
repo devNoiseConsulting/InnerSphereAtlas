7d9f5d61ce41996e63ee32c63d6a48d2796e5d70
Michael J. Flynn <flynnmj@devnoise.com>
2015-02-12 14:09:42 -0500

    Novel Detail conversion

--- lib/novel-detail-publications.php
+++ lib/novel-detail-publications.php
@@ -14,58 +14,14 @@ publisher
 $sth = $dbh->prepare($query);
 $sth->bindParam(':novel', $novel);
 $sth->execute();
-$novelData = $sth->fetchAll(PDO::FETCH_ASSOC);
+$publisherData = $sth->fetchAll(PDO::FETCH_ASSOC);
 $sth = null;
 
-if ($novelData) {
-?>
-<h2>Publication Information:</h2>
-<table border="1" cellspacing="0" cellpadding="5">
-<tr><th>Publisher:</th><th>Stock Number:</th><th>ISBN:</th><th>Year:</th><th>Price:</th><th>Cover:</th><th>Amazon.com:</th></tr>
-<?php
-	for ($i = 0; $i < count($novelData); $i++) {
-		echo "<tr valign=\"top\"><td>";
-		$val = $novelData[$i]['publisher'];
-		print_sp($val);
-		echo "</td>";
-		
-		echo "<td>";
-		$val = $novelData[$i]['stock_id'];
-		print_sp($val);
-		echo "</td>";
-		
-		echo "<td>";
-		$isbn = $novelData[$i]['isbn'];
-		$asin = preg_replace("/-/", "", $isbn);
-		print_sp($isbn);
-		echo "</td>";
-
-		echo "<td>";
-		$val = $novelData[$i]['print_year'];
-		print_sp($val);
-		echo "</td>";
-
-		echo "<td>$";
-		$val = $novelData[$i]['price'];
-		print_sp($val);
-		echo "</td>";
-
-		echo "<td align=\"center\">";
-		$imageFile = $ISA_DOCROOTDIR . "/images/" . $isbn . "-med.jpg";
-		if (file_exists($imageFile)) {
-?>
-<a href="./images/<?php echo $isbn; ?>.jpg"><img src="./images/<?php echo $isbn; ?>-med.jpg" alt="" height="140"></a><br />
-<?php
-		}
-		echo "</td>";
-
-		echo "<td>";
-		$availability = $novelData[$i]['availability'];
-		include("$ISA_LIBDIR/amazon-link.php");
-		echo "</td></tr>\n";
+foreach ($publisherData as $key => $novelPrinting) {
+	$publisherData[$key]['asin'] = preg_replace("/-/", "", $novelPrinting['isbn']);
+	$imageFile = "/images/" . $novelPrinting['isbn'] . "-med.jpg";
+	if (file_exists($ISA_DOCROOTDIR . $imageFile)) {
+		$publisherData[$key]['thumbnail'] = $imageFile; 
+		$publisherData[$key]['imageLink'] = "/images/" . $novelPrinting['isbn'] . ".jpg";
 	}
-?>
-</table>
-<?php
 }
-?>
