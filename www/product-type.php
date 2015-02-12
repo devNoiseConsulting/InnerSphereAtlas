e393cc107b4a31cdd9c33799d240b533e7266cdf
Michael J. Flynn <flynnmj@devnoise.com>
2015-02-12 12:34:50 -0500

    Adding a file that should already be in git.
    Git weirdness seem deleted the file instead of tracking the change. Not
    sure how I made that happen

---
+++ www/product-type.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('../vendor/autoload.php');
+
+require_once("./isatlas-config.php");
+require_once("$ISA_LIBDIR/connect.php");
+
+include("$ISA_LIBDIR/product-type-overview.php");
+
+$loader = new Twig_Loader_Filesystem($ISA_TEMPLATEDIR);
+$twig = new Twig_Environment($loader);
+
+$template = $twig->loadTemplate('product-type.html');
+
+echo $template->render(array(
+	'products' => $products,
+	'nextLink' => $nextLink,
+	'previousLink' => $previousLink,
+	'pageLinks' => $pageLinks,
+	'trimmedsearchvalue' => $trimmedsearchvalue,
+	));
+
