a6ac8af3c0d726cc931ab2bd62b026aee99a5dc1
Michael J. Flynn <flynnmj@devnoise.com>
2015-02-12 14:10:56 -0500

    File copy cause original to be new file.

---
+++ www/novel-detail.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once('../vendor/autoload.php');
+
+require_once("./isatlas-config.php");
+require_once("$ISA_LIBDIR/connect.php");
+
+include("$ISA_LIBDIR/novel-detail.php");
+include("$ISA_LIBDIR/novel-detail-publications.php");
+include("$ISA_LIBDIR/novel-detail-timeline.php");
+
+$loader = new Twig_Loader_Filesystem($ISA_TEMPLATEDIR);
+$twig = new Twig_Environment($loader);
+
+$template = $twig->loadTemplate('novel-detail.html');
+
+echo $template->render(array(
+	'novelData' => $novelData,
+	'publisherData' => $publisherData,
+	'timeline' => $timeline,
+	));
