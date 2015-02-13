b61fd94c7a0226601ce786bec4a0e67ecf892685
Michael J. Flynn <flynnmj@devnoise.com>
2015-02-13 14:50:34 -0500

    Delete unused file
    Most files were converted to Twig templates and no longer need PHP code
    to work.

--- www/legend.php
+++
@@ -1,5 +0,0 @@
-<table>
-<tr><td><img src="./images/fluff.gif" alt="Description" width="16" height="16" border="0" align="left" /></td><td><p>Planet has description information.</p></td></tr>
-<tr><td><img src="./images/factory.gif" alt="Factory" width="16" height="16" border="0" align="left" /></td><td><p>Planet has one of more factories.</p></td></tr>
-</table>
-<br />
