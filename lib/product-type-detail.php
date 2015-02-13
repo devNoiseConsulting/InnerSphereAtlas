9e08b3eeb1067ea5b2e162bd045606eaf7637518
Michael J. Flynn <flynnmj@devnoise.com>
2015-02-13 14:03:15 -0500

    Product Type Detail conversion
    Also a bug in the links on the product type overview page.

--- lib/product-type-detail.php
+++ lib/product-type-detail.php
@@ -1,13 +1,5 @@
 <?php
 
-Function print_sp($val) {
-	$val = chop($val);
-	if($val != "") 
-		print $val;
-	else
-		print "&nbsp;";
-}
-
 $func = array_key_exists("func", $_REQUEST) ? $_REQUEST["func"] : "search";
 $searchvalue = array_key_exists("searchvalue", $_REQUEST) ? $_REQUEST["searchvalue"] : "12000";
 $start = array_key_exists("start", $_REQUEST) ? $_REQUEST["start"] : "0";
@@ -39,15 +31,6 @@ $product = $sth->fetch(PDO::FETCH_ASSOC);
 $sth = null;
 
 $found = $product['found'] - 1;
-
-//echo "<b>Product/Component Type:</b> ";
-echo "<h2>";
-$val = $product['component_type'];
-print_sp($val);
-echo " (";
-$val = $product['product_type'];
-print_sp($val);
-echo ")</h2>\n";
 
 $query = "SELECT DISTINCT
 PC.product_name,
@@ -81,34 +64,3 @@ $products = $sth->fetchAll(PDO::FETCH_ASSOC);
 $sth = null;
 
 include("$ISA_LIBDIR/next_prev.php"); 
-?>
-<table border="1" cellspacing="0" cellpadding="5">
-<tr><th>Product/Component Name:</th><th>Factory:</th><th>Planet:</th></tr>
-<?php
-/* Loop through each item */
-for ($i = 0; $i < count($products); $i++) {
-	echo "<tr><td>";
-	$val = $products[$i]['product_name'];
-	print_sp($val);
-	echo "</td>";
-
-	echo "<td><a href=\"./factory-detail.php?factory=",urlencode($products[$i]['factory_id']),"\">";
-	$val = $products[$i]['factory_name'];
-	print_sp($val);
-	echo "</a></td>";
-
-	echo "<td><a href=\"./planet-detail.php?planet=",urlencode($products[$i]['planet_id']),"\">";
-	$val = $products[$i]['planet_name'];
-	print_sp($val);
-	echo "</a></td>";
-
-	echo "</tr>\n";
-}
-
-?>
-</table>
-<?php
-
-include("$ISA_LIBDIR/next_prev.php"); 
-
-?>
