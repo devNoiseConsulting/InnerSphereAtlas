ac7d041600eb81f3de09023ba841b8b39a68dc10
Michael J. Flynn <flynnmj@devnoise.com>
2015-06-12 23:00:29 -0400

    System Map Fix
    The “&amp;” in the url to get the svg file corrupted the parameter name
    that would tell the code what era to use. Changing to “&” fixes it.

--- www/system-map.php
+++ www/system-map.php
@@ -39,7 +39,7 @@ $era = preg_replace($preg_eras, "\\1", $era);
 
 $eras = array('E2575', 'E2750', 'E3025', 'E3030', 'E3040', 'E3052', 'E3057', 'E3062');
 
-$svg_url = "./system-map-svg.php?planet=" . urlencode($planet) . "&amp;era=" . $era;
+$svg_url = "./system-map-svg.php?planet=" . urlencode($planet) . "&era=" . $era;
 
 $loader = new Twig_Loader_Filesystem($ISA_TEMPLATEDIR);
 $twig = new Twig_Environment($loader);
