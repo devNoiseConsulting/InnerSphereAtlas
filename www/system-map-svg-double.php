ca83429742ee07f9eb977f085560717f5ba08002
Michael J. Flynn <flynnmj@devnoise.com>
2015-06-15 10:40:40 -0400

    Double Jump Tweak
    Getting the code of the double jump svg to closer match the single jump
    sag code.

--- www/system-map-svg-double.php
+++ www/system-map-svg-double.php
@@ -8,14 +8,14 @@ header("Content-type: image/svg+xml");
 print('<?xml version="1.0" encoding="UTF-8" ?>' . "\n");
 ?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1 Basic//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11-basic.dtd">
-<svg 
+<svg
      version="1.1"
      baseProfile="basic"
      xmlns="http://www.w3.org/2000/svg"
      xmlns:xlink="http://www.w3.org/1999/xlink"
      id="svg-root"
-     width="100%" 
-     height="100%" 
+     width="100%"
+     height="100%"
      viewBox="0 0 <?php echo($ISA_MAP_SIZE . ' ' . $ISA_MAP_SIZE); ?>" >
 <?php
 
@@ -24,7 +24,9 @@ if (!is_numeric($planet)) { $planet = 2266787; }
 
 $era = array_key_exists("era", $_REQUEST) ? $_REQUEST["era"] : "3062";
 if (!is_numeric($era)) { $era = 3062; }
-$era = preg_replace("/(2575|2750|30(25|30|40|52|57|62))/", "era_\\1", $era);
+$preg_eras = "/(2575|2750|30(25|30|40|52|57|62))/";
+if (!preg_match($preg_eras, $era)) { $era = 3062; }
+$era = preg_replace($preg_eras, "era_\\1", $era);
 
 $query = "SELECT
 P.planet_id,
@@ -53,8 +55,15 @@ $sth = null;
 if ($planetData) {
 	$name = $planetData['name'];
 	$planet_id = $planetData['planet_id'];
+
 	$x = $planetData['x_coord'];
+	$x_min = $x - 60;
+	$x_max = $x + 60;
+
 	$y = $planetData['y_coord'];
+	$y_min = $y - 60;
+	$y_max = $y + 60;
+
 	$red = $planetData['color1_r'];
 	$green = $planetData['color1_g'];
 	$blue = $planetData['color1_b'];
@@ -81,7 +90,7 @@ if ($planetData) {
 	P2.x_coord,
 	P2.y_coord
 	";
-	
+
 	$sth = $dbh->prepare($query);
 	$sth->bindParam(':planet_id', $planet_id);
 	$sth->execute();
@@ -94,7 +103,7 @@ if ($planetData) {
 <radialGradient id="jumpradial">
 <stop offset="0%" style="stop-color:White;" />
 <stop offset="100%" style="stop-color:rgb(<?php echo($rgb); ?>);">
-<animate attributeName="offset" from="0%" to="100%" dur="10s" repeatCount="indefinite" />
+<animate attributeName="offset" from="0%" to="100%" dur="30s" repeatCount="indefinite" />
 </stop>
 </radialGradient>
 </defs>
@@ -105,20 +114,20 @@ if ($planetData) {
 	$jump = array();
 	/* Loop through each item */
 	for ($i=0; $i < count($planetJumps); $i++) {
-	
+
 		$x1_coord = ($planetJumps[$i]['P1x'] - $x) * $ISA_MAP_SCALE + $ISA_MAP_OFFSET;
 		$y1_coord = ($planetJumps[$i]['P1y'] - $y) * -1 * $ISA_MAP_SCALE + $ISA_MAP_OFFSET;
 		$x2_coord = ($planetJumps[$i]['P2x'] - $x) * $ISA_MAP_SCALE + $ISA_MAP_OFFSET;
 		$y2_coord = ($planetJumps[$i]['P2y'] - $y) * -1 * $ISA_MAP_SCALE + $ISA_MAP_OFFSET;
-	
+
 		$key1 = $x1_coord . "_" . $y1_coord . ":" . $x2_coord . "_" . $y2_coord;
 		$key2 = $x2_coord . "_" . $y2_coord . ":" . $x1_coord . "_" . $y1_coord;
-		
+
 		if (($jump[$key1] == null) || ($jump[$key2] == null)) {
 			echo '<line x1="' . $x1_coord . '" y1="' . $y1_coord;
 			echo '" x2="' . $x2_coord . '" y2="' . $y2_coord;
 			echo '" />' . "\n";
-			
+
 			$jump[$key1] = 1;
 			$jump[$key2] = 1;
 		}
@@ -139,41 +148,44 @@ if ($planetData) {
 	faction F,
 	" . $era . " E
 	WHERE
-	(P.x_coord > " . ($x - 60) . " AND P.x_coord < " . ($x + 60) . ") AND
-	(P.y_coord > " . ($y - 60) . " AND P.y_coord < " . ($y + 60) . ") AND
+	(P.x_coord > :x_min AND P.x_coord < :x_max) AND
+	(P.y_coord > :y_min AND P.y_coord < :y_max) AND
 	P.planet_id = E.planet_id AND
 	E.faction_id = F.faction_id
 	ORDER BY
 	P.x_coord,
 	P.y_coord
 	";
-	
+
 	$sth = $dbh->prepare($query);
-	$sth->bindParam(':planet_id', $planet_id);
+	$sth->bindParam(':x_min', $x_min);
+	$sth->bindParam(':x_max', $x_max);
+	$sth->bindParam(':y_min', $y_min);
+	$sth->bindParam(':y_max', $y_max);
 	$sth->execute();
 	$planets = $sth->fetchAll(PDO::FETCH_ASSOC);
 	$sth = null;
-	
+
 	$font_size = $ISA_MAP_PLANET_DIAMETER * 1.5;
 	echo '<g font-family="Helvetica" font-size="' . $font_size . '" >' . "\n";
 	/* Loop through each item */
 	for ($i=0; $i < count($planets); $i++) {
 		$name = $planets[$i]['name'];
 		$planet_id = $planets[$i]['planet_id'];
-	
+
 		$x_coord = ($planets[$i]['x_coord'] - $x) * $ISA_MAP_SCALE + $ISA_MAP_OFFSET;
 		$y_coord = ($planets[$i]['y_coord'] - $y) * -1 * $ISA_MAP_SCALE + $ISA_MAP_OFFSET;
-	
+
 		$red = $planets[$i]['color1_r'];
 		$green = $planets[$i]['color1_g'];
 		$blue = $planets[$i]['color1_b'];
 		$color = 'rgb(' . $red . ',' . $green . ',' . $blue . ')';
-		
+
 		$capital = $planets[$i]['capital'];
-	
+
 		$x_title = $x_coord - $ISA_MAP_TITLE_OFFSET + 1;
 		$y_title = $y_coord + $font_size + 2;
-		
+
 		echo '<a xlink:href="./planet-detail.php?planet=' . $planet_id . '" xlink:alt="' . $name . '" target="_top">' . "\n";
 		if ($capital == 0) {
 			echo '<circle id="jump_limit" cx="' . $x_coord . '" cy="' . $y_coord . '" r="' . $ISA_MAP_PLANET_RADIUS;
@@ -181,7 +193,7 @@ if ($planetData) {
 		} else {
 			echo '<polygon stroke="' . $color . '" stroke-width="1.5" ';
 			echo 'fill="' . $color . '" points="';
-			
+
 			$radius = ((1 / $capital) + 1) * $ISA_MAP_PLANET_RADIUS;
 			$sides = $capital + 2;
 			$angle = 2 * pi() / $sides;
@@ -195,7 +207,7 @@ if ($planetData) {
 				echo round($pX, 2) . "," . round($pY, 2) . " ";
 				$currentAngle += $angle;
 			}
-			
+
 			echo '" />';
 			$x_title -= ($radius / 4);
 			$y_title += ($radius / 4);
