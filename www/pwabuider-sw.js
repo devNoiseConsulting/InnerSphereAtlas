77d0b5631a90b15a92ad2636ce7d6cd590aa9909
Michael J. Flynn <flynnmj@devnoise.com>
2018-04-04 21:37:20 -0400

    Content Security Protocol
    Changes to the CSP so the service worker can fetch content.

--- www/pwabuider-sw.js
+++ www/pwabuider-sw.js
@@ -1,11 +1,14 @@
 //This is the "Offline page" service worker
 
+const cacheVersion = 1;
+const staticCacheName = `pwabuilder-offline-${cacheVersion}`;
+
 //Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener('install', function(event) {
   var offlinePage = new Request('offline.html');
   event.waitUntil(
   fetch(offlinePage).then(function(response) {
-    return caches.open('pwabuilder-offline').then(function(cache) {
+    return caches.open(staticCacheName).then(function(cache) {
       console.log('[PWA Builder] Cached offline page during Install'+ response.url);
       return cache.put(offlinePage, response);
     });
@@ -18,7 +21,7 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function(error) {
         console.error( '[PWA Builder] Network request Failed. Serving offline page ' + error );
-        return caches.open('pwabuilder-offline').then(function(cache) {
+        return caches.open(staticCacheName).then(function(cache) {
           return cache.match('offline.html');
       });
     }));
@@ -26,7 +29,7 @@ self.addEventListener('fetch', function(event) {
 
 //This is a event that can be fired from your page to tell the SW to update the offline page
 self.addEventListener('refreshOffline', function(response) {
-  return caches.open('pwabuilder-offline').then(function(cache) {
+  return caches.open(staticCacheName).then(function(cache) {
     console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
     return cache.put(offlinePage, response);
   });
